7ca116919b9a119cf4821f31ca5f03121bca4f09
Aaron_Kubiesa <31861979+sageroe@users.noreply.github.com>
2020-01-08 20:38:41 +0100

    Add files via upload
    Aktualizacja z obrazkiem "failed"

--- Projekt 2/main.py
+++ Projekt 2/main.py
@@ -14,22 +14,26 @@ def get_image(path):
         image = _image_library.get(path)
         if image == None:
                 canonicalized_path = path.replace('/', os.sep).replace('\\', os.sep)
-                image = pygame.image.load(canonicalized_path)
+                image = pygame.image.load(canonicalized_path).convert()
                 _image_library[path] = image
         return image
+
+
 def main():
+
     pygame.init()
     screen = pygame.display.set_mode((640,480))
     running = True
     clock = pygame.time.Clock()
     #lista muzyki do plakatu avengersów
     _songs_avengers = ['avengers']
-
+    fail_image = pygame.image.load('failed.jpg')
     _currently_playing_song = None
     #ustalamy zdarzenie które ma zakończyć granie muzyki
     SONG_END = pygame.USEREVENT + 1
 
     pygame.mixer.music.set_endevent(SONG_END)
+    #Ładujemy wszystkie niespędne pliki z muzyką poniżej. Za żadne skarby nie rób tego w pętli
     pygame.mixer.music.load('avengers.mp3')
 
     START = False
@@ -41,16 +45,23 @@ def main():
                 running = False
             #poniższy kod wykona się kiedy wciśniemy SPACE
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                     pygame.mixer.music.play()
                      START = True
             #kod poniżej wykona się kiedy dojdzie do zakończenia odtwarzania dźwięku
-            if event.type == SONG_END:
-                print("songs end has come")
-            #wypełniamy okno kolorami i obrazkami poniżej                
             screen.fill((255,255,255))
+            if event.type == SONG_END:
+                START = False
+                print("Na na na na koniec piosenki")
+                screen.blit(fail_image,(10,30))
+            #wypełniamy okno kolorami i obrazkami poniżej
+
             if START:
                 screen.blit(get_image('avengers.jpg'),(100,30))
+                pygame.display.flip()
+                if pygame.mixer.music.get_busy() == False:
+                    pygame.time.wait(3000)
+                    pygame.mixer.music.play(1)
 
-        pygame.display.flip()
+            if not START:
+                    pygame.display.flip()
 if __name__=="__main__":
     main()
